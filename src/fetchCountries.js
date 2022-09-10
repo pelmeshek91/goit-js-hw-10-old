ca3c1683e6b9e1cc1eee5d2db7665a709a443422
Lyuba Pushkina <lyuba.pushkina@gmail.com>
2022-09-10 16:48:25 +0100

    update

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,21 +1,19 @@
-const BASE_URL = 'https://restcountries.com/v3.1/';
-const character = 'character';
+const BASE_URL = 'https://restcountries.com/v3.1/all';
+/* const character = 'character';
 const limit = 10;
 const options = {
   headers: {
     Authorization: bearer,
   },
-};
+}; */
 
-export default function lordApi(page = 1) {
-  const response = fetch(
-    `${BASE_URL}${character}/?limit=${limit}&page=${page}`,
-    options
-  )
+export default function fetchCountries() {
+  const response = fetch(`${BASE_URL}`)
     .then(data => {
       if (!data.ok) {
         throw new Error('Fail');
       }
+
       return data.json();
     })
     .catch(err => console.error(err));
